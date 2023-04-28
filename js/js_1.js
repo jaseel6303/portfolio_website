39533db3bf5081551fa17d3ecab91912593929b6
jaseel6303 <jaseeljasee1997@gmail.com>
2023-04-28 20:32:25 +0530

    navabr

---
+++ js/js_1.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 550) {
+            $('.navbar').addClass('solid');
+            $('.back-to-top').addClass('visible');
+        } else {
+            $('.navbar').removeClass('solid');
+            $('.back-to-top').removeClass('visible');
+        }
+
+    });
+});
+
